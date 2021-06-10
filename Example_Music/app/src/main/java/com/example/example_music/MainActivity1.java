e438bd67eac191dc4b4fd10d8910f72aad4f0327
Nikhil <nikilvarma314@gmail.com>
2021-06-10 18:13:40 +0530

    09-06-21

---
+++ Example_Music/app/src/main/java/com/example/example_music/MainActivity1.java
@@ -0,0 +1,13 @@
+package com.example.example_music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
